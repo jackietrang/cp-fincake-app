67539da8292f1677a6067e937a34fc5f8c3b6dc5
Jackie Nguyen <jackienguyen@Jackies-MacBook-Pro.local>
2022-02-13 16:49:17 -0800

    file structure

---
+++ static/flashcard/js/cards_entry.js
@@ -0,0 +1,57 @@
+flashcardDeck = {
+    title: "Entry Test Flashcards",
+    catchPhrase: "Financial literacy made simple",
+    cards: [{
+            "question": "A profit is a positive return or money made on an investment or venture",
+            "answer": "True"
+        },
+        {
+            "question": "Suppose you had $100 in a savings account and the interest rate was 2% per year. After 5 years, how much do you think you would have in the account if you left the money to grow?",
+            "answer": "More than $102. Ignoring interest compounding, borrowing at 20 percent per year would lead to doubling in ve years; someone who knew about\
+            interest on interest might have selected a number less than ve.Someone who knows the 'rule of 72'\
+            heuristic would know that it would be about 3.6 years, which makes the correct answer '2 to 4 years'.\
+            In finance, the rule of 72 is a method for estimating an investment's doubling time. The rule number (i.e., 72) is divided by the interest percentage per period to obtain the approximate\
+            number of periods(usually years) required for doubling.The other responses reect a misunderstanding of the concept of interest accrual."
+        },
+        {
+            "question": "Imagine that the interest rate on your savings account was 1% per year and inflation was 2% per year. After 1 year, with the money in this account, would you be able to buy... ",
+            "answer": "Less than today. The reason you have less is ination. Ination is the rate at which the price of goods and services rises. If the annual ination rate is 2\
+            percent but the savings account only earns 1 percent,\
+            the cost of goods and services has outpaced the buying power of the money in\
+            the savings account that year.Put another way,\
+            your buying power has not kept up with ination."
+        },
+        {
+            "question": "If interest rates rise, what will typically happen to bond prices?",
+            "answer": "Fall. When interest rates rise, bond prices fall. And when interest rates fall, bond prices rise. This is because as interest rates go up, newer\
+            bonds come to market paying higher interest yields than older bonds already in the hands of investors,\
+            making the older bonds worthless."
+        },
+        {
+            "question": "A broker advises you to invest in ABC Corp, which she owns. Is she acting as your fiduciary?",
+            "answer": "They may not because they personally benefit from you investing in that company"
+        },
+        {
+            "question": "Which of the following is an example of an equity calculation?",
+            "answer": "$200,000 main residence - $100,000 main residence loan"
+        },
+        {
+            "question": "A 15-year mortgage typically requires higher monthly payments than a 30-year mortgage, but the total interest paid over the life of the loan will be less.",
+            "answer": "True. Assuming the same interest rate for both loans, you will pay less in interest over the life of a 15-year loan than you would with a 30-year loan because\
+            you repay the principal at a faster rate.This also explains why the monthly payment\
+            for a 15-year loan is higher. Let's say youget a 30-year mortgage at 6 percent on a $150,000 home.\
+            You will pay $899 a month in principal and interest charges.\
+            Over 30 years, you will pay $173, $757 in interest alone. But a 15-year mortgage at the same rate will cost you less. You will pay $1,266\
+            each month but only $77,841 in total interest— nearly $100,000 less."
+        },
+        {
+            "question": "Buying a single company’s stock usually provides a safer return than a stock mutual fund.",
+            "answer": "False. In general, investing in a stock mutual fund is less risky than investing in a single stock because mutual funds oer a way to diversify.\
+            Diversication means spreading your risk by spreading your investments. With a single stock,\
+            all your eggs are in one basket. If the price falls when you sell,\
+            you lose money.With a mutual fund that invests in the stocks of dozens (or even hundreds) of companies,\
+            you lower the chances that a price decline for any single stock will impact your\
+            return. Diversication generally may result in a more consistent performance in dierent market conditions."
+        }
+    ]
+};
